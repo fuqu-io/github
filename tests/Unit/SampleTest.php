cce9707519b4334b85c2e15b483c2c4339fd4283
Tarek Adam <tarek.adam@gmail.com>
2020-03-09 07:58:40 -0600

    redo test suite strategy

---
+++ tests/Unit/SampleTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace FuquPackage\Tests\Unit;
+
+use Tests\TestCase;
+
+class SampleTest extends TestCase{
+
+	/** @test */
+	public function sample_test(){
+		
+		$this->assertTrue(true);
+		
+	}
+
+}
+
+
+
+
+
